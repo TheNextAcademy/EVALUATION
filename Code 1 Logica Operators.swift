212026acf382c007e12983cf4c1bbd54a63b5f30
Next Academy <48500601+TheNextAcademy@users.noreply.github.com>
2022-01-23 21:29:34 -0500

    Update Code 1 Logica Operators.swift

--- Code 1 Logica Operators.swift
+++ Code 1 Logica Operators.swift
@@ -1,1 +1,47 @@
 
+//---------------------------------------------------
+//1. Is this code correct?  If it is not then explain why.
+
+if isOnClosedSwitch && isBlockedRight | isOnGem
+   print("Toggle the switch and get the gem")
+
+
+
+
+
+
+
+//---------------------------------------------------
+//2. Is this code correct?  If it is not then explain why.
+
+if (isBlocked && isBlockedRight && isBlockedLeft){
+   print("I am completely blocked.")
+}
+
+
+
+
+
+
+
+//---------------------------------------------------
+//3. Is this code correct?  If it is not then explain why.
+
+if isOnGem & !isOnGem & isBlocked {
+   print("Let's get the gem and move forward.")
+}
+
+
+
+
+
+
+
+//---------------------------------------------------
+//4. Is this code correct?  If it is not then explain why.
+
+if  {
+   isBlocked && isOnGem
+   print("Let's get the gem and move forward.")
+}
+
