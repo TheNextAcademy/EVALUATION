b22093e363c8bc0bffea17993238ac0b593e2e6c
Next Academy <48500601+TheNextAcademy@users.noreply.github.com>
2022-01-23 20:55:44 -0500

    Create Code 1 For Loops.swift

---
+++ Code 1 For Loops.swift
@@ -0,0 +1,9 @@
+//---------------------------------------------------
+//Is this code correct?  If it is not then explain why it is.
+
+for i in 1 ... 10{
+    for j in 1 ... 20{
+        print("many many loops")
+    }
+}
+
