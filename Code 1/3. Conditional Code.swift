c9c0a36ce68b7ca6260f83f6dc0c70c19af376af
Next Academy <48500601+TheNextAcademy@users.noreply.github.com>
2022-01-23 21:36:55 -0500

    Create 3. Conditional Code.swift

---
+++ Code 1/3. Conditional Code.swift
@@ -0,0 +1,66 @@
+
+//---------------------------------------------------
+//1. Is this code correct?  If it is not then explain why.
+
+if isSunny{
+  else if isRaining{
+    print("Where is my umbrella?")
+  }
+  print("Let's go out an play")
+}
+
+
+
+
+
+
+
+//---------------------------------------------------
+//2. Is this code correct?  If it is not then explain why.
+
+if isHungry{
+  print("Eat something!")
+}else if isTired{
+  print("Get some sleep")
+}else if isBored{
+  print("Play something)
+}else{
+    print("Do nothing")
+  }
+
+
+
+
+
+
+
+//---------------------------------------------------
+//3. Is this code correct?  If it is not then explain why.
+
+else if lateToSchool{
+  print("Let's run to school")
+}else if forgotHomework{
+  print("Let's ask teacher for an extension.")
+}if isAllGood{
+  print("School is the best!")
+}
+
+
+
+
+
+
+
+//---------------------------------------------------
+//4. Is this code correct?  If it is not then explain why.
+
+if isBlocked{
+   if isBlockedRight{
+     if isBlockedLeft{
+       print("Turn right")
+     }
+     print("Turn left")
+   }
+  print("turn back")
+}
+
