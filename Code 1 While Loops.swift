bd236a37c453a9b701ddaeccf7865c15590e1d94
Next Academy <48500601+TheNextAcademy@users.noreply.github.com>
2022-01-23 21:21:37 -0500

    Update and rename While Loops.swift to Code 1 While Loops.swift

---
+++ Code 1 While Loops.swift
@@ -0,0 +1,43 @@
+
+//---------------------------------------------------
+//1. Is this code correct?  If it is not then explain why it is.
+
+while isBlocked
+  moveForward()
+  moveForward()
+  moveForward()
+  moveForward()
+  
+
+
+//---------------------------------------------------
+//2. Is this code correct?  If it is not then explain why it is.
+
+while isOnGem {
+ collectGem()
+ moveForward()
+}
+
+
+
+
+//---------------------------------------------------
+//3. Is this code correct?  If it is not then explain why it is.
+
+
+while !isBlocked{
+  print("Let's move forward") 
+}
+
+
+
+
+
+//---------------------------------------------------
+//4. Is this code correct?  If it is not then explain why it is.
+
+
+{
+  while !isOnswitch
+  print("Let's move forward") 
+}
