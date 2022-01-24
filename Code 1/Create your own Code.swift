bbba08993e23513d819d969537c2b531bf536087
Next Academy <48500601+TheNextAcademy@users.noreply.github.com>
2022-01-23 21:43:36 -0500

    Create Create your own Code.swift

---
+++ Code 1/Create your own Code.swift
@@ -0,0 +1,17 @@
+//1. Functions
+//Create a function that will print your name 10 times.
+//Use a loop inside of the function.
+//Dont forget to call the function in the end.
+
+
+
+
+//2. Functions with conditions.
+//Write a function that will check if something is human and alive or not. 
+// use isHuman and isAlive to white a condition in the function.  
+// If both are true then print("It is a human.").
+// If isHuman is true but isAlive is false then print("Sorry but the human is dead.")
+// If isHuman is false but isAlive is true then print("It's an alien!")
+// If both isHuman and isAlive is false then print("Looks like the alien is not moving.")
+var isHuman = Bool.random()
+var isAlive = Bool.random()
